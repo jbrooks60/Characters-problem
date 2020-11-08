2421ec5dc2605be9aa8bc712ca52e6bc1998bdee
jbrooks6 <jbrooks6@student.strode-college.ac.uk>
2020-11-08 20:11:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Characters problem
+
+# Defining components of aplhanumerics 
+
+Digits = ("0123456789")
+
+Characters = ("abcdABCD@#!£")
+
+# Main program
+
+Alphanumerics = (Digits, Characters)
+
+print (Alphanumerics)
